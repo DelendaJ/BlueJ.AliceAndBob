8ff7e2e05bbe06388b31a6a51b4bc39766b6da8c
Delenda Joseph <delendajoseph@zipcoders-MacBook-Pro.local>
2019-02-06 11:58:12 -0500

    Fun with Alice and Bob

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
-/**
+ /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args ){
-
-    }
+    String name = "Alice";
+    String name2 = "Bob";
+    Scanner input = new Scanner (System.in);
+    System.out.print("Name please: ");
+    String fname = input.next();
+    if (fname.equals(name) || fname.equals(name2)) {
+     System.out.println("Hello " +fname+" ");
+    } else {
+        System.out.println("I know what you did, " +fname+"!");
+        
 }
+}   }
